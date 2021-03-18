00ba780326f172129aa5de59932974cdc40f55a2
github lion-king-IT <reo.runningg@gmail.com>
2021-03-18 10:25:40 +0900

    トップ画面の構造を作る

--- app/src/main/java/com/reo/running/yumemitask/screen/DetailsFragment.kt
+++ app/src/main/java/com/reo/running/yumemitask/screen/DetailsFragment.kt
@@ -1,4 +1,17 @@
 package com.reo.running.yumemitask.screen
 
-class DetailsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DetailsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- app/src/main/java/com/reo/running/yumemitask/screen/ListFragment.kt
+++ app/src/main/java/com/reo/running/yumemitask/screen/ListFragment.kt
@@ -1,4 +1,17 @@
 package com.reo.running.yumemitask.screen
 
-class ListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ListFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }

0139f0fbed1de42f9dba0bed8c7abbec579a3614
github lion-king-IT <reo.runningg@gmail.com>
2021-03-20 14:25:40 +0900

    Lottieプラグイン

--- app/src/main/java/com/reo/running/yumemitask/screen/details/DetailsFragment.kt
+++ app/src/main/java/com/reo/running/yumemitask/screen/details/DetailsFragment.kt
@@ -26,5 +26,8 @@ class DetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.run {
+            detailsConstraintLayout.startAnimation()
+        }
     }
 }
